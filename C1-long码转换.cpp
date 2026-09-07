1b16ca8837f4249abe8ad9ab14e3d9eb2709dd53
AllenDing <13127598979@163.com>
2026-09-07 23:14:05 +0800

    第一章long码转换

---
+++ C1-long码转换.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+    using std::cout;using std::endl;using std::cin;
+    cout << "请输入长度" << endl;
+    int Long;
+    cin >> Long;
+    int Ma;
+    Ma = Long * 220;
+    cout << Long << "米等于" << Ma << "码" << endl;
+    return 0;
+}
