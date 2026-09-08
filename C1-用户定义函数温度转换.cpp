e063a4bd78a5335e1db28cdbe59ea74f82264f3d
AllenDing <13127598979@163.com>
2026-09-08 20:39:48 +0800

    第一章用户定义函数温度转换

---
+++ C1-用户定义函数温度转换.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+ using std::cout;using std::cin;using std::endl;
+ int Huashi(int);
+ int main(){
+    int Sheshi;
+    cin>>Sheshi;
+    Huashi(Sheshi);
+    cout<<Sheshi<<"摄氏度等于"<<Huashi(Sheshi)<<"华氏度"<<endl;
+return 0;
+ }
+ int Huashi(int Sheshi){
+    return (Sheshi*9/5)+32;
+ }
