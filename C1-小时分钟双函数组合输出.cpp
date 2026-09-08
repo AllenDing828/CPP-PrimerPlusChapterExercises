f5245d7dcf9ccc2bb4ae845026c35a4abd3196af
AllenDing <13127598979@163.com>
2026-09-08 21:17:40 +0800

    完成最后一道题！甚至调用了三个函数精确到秒！

--- C1-小时分钟双函数组合输出.cpp
+++ C1-小时分钟双函数组合输出.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 using std::cout;using std::cin;using std::endl;
-void Hours(int);void Minutes(int);void Seconds(int);
+void ZuHeShuChu(int hours,int minutes,int seconds);
 int main(){
     int hours, minutes, seconds;
     cout<<"请输入小时数：";cin>>hours;
     cout<<"请输入分钟数：";cin>>minutes;
     cout<<"请输入秒数：";cin>>seconds;
-    Hours(hours);
-    Minutes(minutes);
-    Seconds(seconds);
-    cout<<"当前时间为"<<hours<<"点"<<minutes<<"分"<<seconds<<"秒"<<endl;
+    ZuHeShuChu(hours,minutes,seconds);
+return 0;
 }
-void Hours (int hours){
-    cout << hours ;
-}
-void Minutes (int minutes){
-    cout << minutes ;
-}
-void Seconds (int seconds){
-    cout << seconds ;
-}
+void ZuHeShuChu(int hours,int minutes,int seconds){
+    cout<<"当前时间为："<<hours<<"点"
+    <<minutes<<"分"<<seconds<<"秒"<<endl;
+}   
